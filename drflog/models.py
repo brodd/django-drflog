34942866b002aaf73a1c58e43172a1bd7f038aff
brodd <jimmy.brodd@gmail.com>
2018-01-23 11:43:59 +0100

    Add db indices on searchable columns

--- drflog/models.py
+++ drflog/models.py
@@ -13,15 +13,15 @@ class Entry(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              null=True)
 
-    ip = models.GenericIPAddressField()
+    ip = models.GenericIPAddressField(db_index=True)
 
     host = models.URLField()
-    path = models.CharField(max_length=200)
+    path = models.CharField(max_length=200, db_index=True)
 
-    method = models.CharField(max_length=10)
-    status = models.PositiveSmallIntegerField(null=True)
+    method = models.CharField(max_length=10, db_index=True)
+    status = models.PositiveSmallIntegerField(null=True, db_index=True)
 
-    user_agent = models.CharField(max_length=200)
+    user_agent = models.CharField(max_length=200, db_index=True)
     query_params = JSONField(null=True)
     request_data = JSONField(null=True)
     response_data = JSONField(null=True)
